be87f2f83959b7d43845683edb664623e38afc2b
Eug <eupston130@hotmail.com>
2024-04-10 22:08:02 +0800

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { Providers } from "./providers/providers";
-import { useBalance } from "wagmi";
-import { ShowBalance } from "./components/ShowBalances";
-import { SendTokens } from "./components/SendTokenButton";
 import { Navbar } from "./components/Navbar";
 import { MyWalletInfo } from "./components/MyWalletInfo";
 
 function App() {
   return (
     <Providers>
-      {/* <StyledDiv color={color} onClick={handleClick}>
-            Hello, world!
-          </StyledDiv> */}
-      {/* <SendTokens />
-          <ShowBalance /> */}
-
       <Navbar />
       <MyWalletInfo />
     </Providers>
